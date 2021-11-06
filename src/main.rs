8b19506394df091187079bfd6f4981977f1f9467
Anocha Trachutham <kaysha.ou@gmail.com>
2021-11-06 18:59:04 +0800

    new feature

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::cmp::Ordering;
 fn main() {
     println!("Guess the number!");
 
-    // changes here
+    // changes here this is for new branch change 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
     println!("The secret number is {}", secret_number);
